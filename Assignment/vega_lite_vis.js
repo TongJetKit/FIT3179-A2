672943a97e4e4dbfb93950390f7ea4696abb67ed
TongJetKit <105489337+TongJetKit@users.noreply.github.com>
2023-09-23 23:56:30 +0800

    Add Assignment files

---
+++ Assignment/vega_lite_vis.js
@@ -0,0 +1,4 @@
+var spec2 = "happiness_score_scatterplot.json";
+vegaEmbed('#scatterplot', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
