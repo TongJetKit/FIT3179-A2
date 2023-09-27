afd4d1eefe6b715c4b5d256e70f7b12d2bb02c80
TongJetKit <105489337+TongJetKit@users.noreply.github.com>
2023-09-27 16:24:44 +0800

    Delete Assignment directory

--- Assignment/vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var spec2 = "happiness_score_scatterplot.json";
-vegaEmbed('#scatterplot', spec2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
