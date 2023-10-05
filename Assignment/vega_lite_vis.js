dbd7708c92a6e579af4a219630179e5381e34773
TongJetKit <jton0028@student.monash.edu>
2023-10-06 00:56:47 +0800

    Added year to the tooltip of the choropleth map

--- Assignment/vega_lite_vis.js
+++ Assignment/vega_lite_vis.js
@@ -1,4 +1,52 @@
-var spec2 = "happiness_score_scatterplot.json";
-vegaEmbed('#scatterplot', spec2).then(function(result) {
+var scatter = "https://raw.githubusercontent.com/TongJetKit/FIT3179-A2/main/Assignment/happiness_score_scatterplot.json";
+vegaEmbed('#scatterplot', scatter).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var barchart = "https://raw.githubusercontent.com/TongJetKit/FIT3179-A2/main/Assignment/happiness_score_barchart.json";
+vegaEmbed('#barchart', barchart).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var corruption_map = "https://raw.githubusercontent.com/TongJetKit/FIT3179-A2/main/Assignment/happiness_score_choropleth_corruption.json";
+vegaEmbed('#corruption_map', corruption_map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var freedom_map = "https://raw.githubusercontent.com/TongJetKit/FIT3179-A2/main/Assignment/happiness_score_choropleth_freedom.json";
+vegaEmbed('#freedom_map', freedom_map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var gdp_map = "https://raw.githubusercontent.com/TongJetKit/FIT3179-A2/main/Assignment/happiness_score_choropleth_gdp.json";
+vegaEmbed('#gdp_map', gdp_map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var generosity_map = "https://raw.githubusercontent.com/TongJetKit/FIT3179-A2/main/Assignment/happiness_score_choropleth_generosity.json";
+vegaEmbed('#generosity_map', generosity_map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var life_map = "https://raw.githubusercontent.com/TongJetKit/FIT3179-A2/main/Assignment/happiness_score_choropleth_life.json";
+vegaEmbed('#life_map', life_map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var social_map = "https://raw.githubusercontent.com/TongJetKit/FIT3179-A2/main/Assignment/happiness_score_choropleth_social.json";
+vegaEmbed('#social_map', social_map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var linechart = "https://raw.githubusercontent.com/TongJetKit/FIT3179-A2/main/Assignment/happiness_score_linechart.json";
+vegaEmbed('#linechart', linechart).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var stackedbarchart = "https://raw.githubusercontent.com/TongJetKit/FIT3179-A2/main/Assignment/happiness_score_stacked_barchart.json";
+vegaEmbed('#stackedbarchart', stackedbarchart).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+
